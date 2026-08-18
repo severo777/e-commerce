c253fa32981601d699a0639249bd807cea5ccc31
severo777 <tiagosevero493@gmail.com>
2026-08-17 21:25:53 -0400

    Projeto criado

---
+++ ecormmerce/src/main/java/com/tads/ecormmerce/EcormmerceApplication.java
@@ -0,0 +1,27 @@
+package com.tads.ecormmerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcormmerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcormmerceApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ecormmerce/src/test/java/com/tads/ecormmerce/EcormmerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.ecormmerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcormmerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
